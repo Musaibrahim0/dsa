fd403a47ee60c841a272ab80c452839d3b094f89
hammadmajid <hammadmajid@proton.me>
2024-10-07 07:31:44 +0500

    oct 7 07:31

--- list/main.cpp
+++ list/main.cpp
@@ -2,5 +2,125 @@
 #include <iostream>
 
 int main() {
-    return 0;
+  // Create an instance of the Singly linked list for integers
+  Singly<int> list;
+
+  // Test insert_head
+  list.insert_head(10);
+  list.insert_head(20);
+  list.insert_head(30);
+  std::cout << "After inserting 30, 20, 10 at head:" << std::endl;
+  list.display();
+
+  // Test insert_tail
+  list.insert_tail(40);
+  list.insert_tail(50);
+  std::cout << "After inserting 40, 50 at tail:" << std::endl;
+  list.display();
+
+  // Test insert_at_index
+  list.insert_at_index(25, 2); // Insert 25 at index 2
+  std::cout << "After inserting 25 at index 2:" << std::endl;
+  list.display();
+
+  // Test remove_head
+  list.remove_head();
+  std::cout << "After removing head:" << std::endl;
+  list.display();
+
+  // Test remove_tail
+  list.remove_tail();
+  std::cout << "After removing tail:" << std::endl;
+  list.display();
+
+  // Test remove_index
+  list.remove_index(1); // Remove the node at index 1
+  std::cout << "After removing node at index 1:" << std::endl;
+  list.display();
+
+ // Test size
+  std::cout << "Current size of the list: " << list.get_size() << std::endl;
+
+  return 0;
 }
+
+// int main() {
+//   Singly<int> list;
+
+//   int data;
+//   int option;
+
+//   while (true) {
+//     std::cout << "Available operations:" << std::endl
+//               << "0. Exit" << std::endl
+//               << "1. Display" << std::endl
+//               << "2. Search" << std::endl
+//               << "3. Insert head" << std::endl
+//               << "4. Insert at index" << std::endl
+//               << "5. Insert tail" << std::endl
+//               << "6. Remove head" << std::endl
+//               << "7. Remove index" << std::endl
+//               << "8. Remove tail" << std::endl
+//               << "Your option: ";
+
+//     std::cin >> option;
+
+//     switch (option) {
+//     case 0:
+//       return 0;
+
+//     case 1:
+//       list.display();
+//       break;
+
+//     case 2:
+//       std::cout << "Enter data: ";
+//       std::cin >> data;
+
+//       std::cout << list.search(data);
+//       break;
+
+//     case 3:
+//       std::cout << "Enter data: ";
+//       std::cin >> data;
+
+//       list.insert_head(data);
+//       break;
+
+//     case 4:
+//       std::cout << "Enter index: ";
+//       std::cin >> option;
+
+//       std::cout << "Enter data: ";
+//       std::cin >> data;
+
+//       list.insert_at_index(data, option);
+//       break;
+
+//     case 5:
+//       std::cout << "Enter data: ";
+//       std::cin >> data;
+
+//       list.insert_tail(data);
+//       break;
+
+//     case 6:
+//       list.remove_head();
+//       break;
+
+//     case 7:
+//       std::cout << "Enter index: ";
+//       std::cin >> option;
+
+//       list.remove_index(option);
+//       break;
+
+//     case 8:
+//       list.remove_tail();
+//       break;
+
+//     default:
+//       std::cout << "Invalid option" << std::endl;
+//     }
+//   }
+// }
--- list/singly.h
+++ list/singly.h
@@ -2,37 +2,157 @@
 #define LIST_LIBRARY_H
 #include <iostream>
 
-template<typename T>
-class Singly {
-    struct Node {
-        T data;
-        Node *next;
-    } *head;
+template <typename T> class Singly {
+  struct Node {
+    T data;
+    Node *next;
+  } *head;
 
-    unsigned size{};
+  unsigned size;
 
 public:
-    Singly() : head(nullptr), size(0) {
+  Singly() : head(nullptr), size(0) {}
+
+  ~Singly() {
+    Node *current = head;
+    while (current) {
+      Node *temp = current;
+      current = current->next;
+      delete temp;
+    }
+  }
+
+  void insert_head(T data) {
+    Node *new_node = new Node;
+    new_node->data = data;
+
+    if (head == nullptr) {
+      new_node->next = nullptr;
+      head = new_node;
+      size++;
+      return;
     }
 
+    new_node->next = head;
+    head = new_node;
+    size++;
+  }
 
-    /**
-    * Checks the total number of element in the list.
-    *
-    * @return The total number of elements
-     */
-   unsigned get_size() const { return size; } // NOLINT(*-use-nodiscard)
+  void insert_tail(T data) {
+    Node *new_node = new Node;
+    new_node->data = data;
+    new_node->next = nullptr;
 
-    /**
-    * Checks if the linked list is empty or not.
-    *
-    * @return true if empty else false
-     */
-    bool is_empty() {
-        if (head == nullptr)
-            return true;
-        return false;
+    if (head == nullptr) {
+      head = new_node;
+      size++;
+      return;
     }
+
+    Node *last = head;
+
+    while (last->next != nullptr) {
+      last = last->next;
+    }
+
+    last->next = new_node;
+
+    size++;
+  }
+
+  void insert_at_index(T data, unsigned position) {
+    if (position > size)
+      return; // Out of bounds
+
+    if (position == 0) {
+      insert_head(data);
+      return;
+    }
+
+    Node *new_node = new Node{data, nullptr};
+    Node *current = head;
+
+    for (unsigned i = 0; i < position - 1; i++) {
+      current = current->next;
+    }
+
+    new_node->next = current->next;
+    current->next = new_node;
+    size++;
+  }
+
+  void remove_head() {
+    if (head == nullptr)
+      return;
+
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+    size--;
+  }
+
+  void remove_tail() {
+    if (head == nullptr)
+      return; // Empty list
+
+    if (head->next == nullptr) {
+      delete head;
+      head = nullptr;
+    } else {
+      Node *current = head;
+      while (current->next->next != nullptr) {
+        current = current->next;
+      }
+      delete current->next;
+      current->next = nullptr;
+    }
+    size--;
+  }
+  void remove_index(unsigned position) {
+    if (position >= size)
+      return; // Out of bounds
+
+    if (position == 0) {
+      Node *temp = head;
+      head = head->next;
+      delete temp;
+    } else {
+      Node *current = head;
+      for (unsigned i = 0; i < position - 1; i++) {
+        current = current->next;
+      }
+      Node *temp = current->next;
+      current->next = temp->next;
+      delete temp;
+    }
+    size--;
+  }
+
+  long int search(T data) {
+    Node *current = head;
+    while (current != nullptr) {
+      if (current->data == data) {
+        return current->data;
+      }
+    }
+    return -1;
+  }
+
+  void display() {
+    Node *current = head;
+
+    std::cout << "size: " << size << std::endl;
+
+    while (current != nullptr) {
+      std::cout << current->data << "-->";
+      current = current->next;
+    }
+    std::cout << "nullptr" << std::endl;
+
+    delete current;
+  }
+
+  unsigned get_size() { return size; }
 };
 
-#endif //LIST_LIBRARY_H
+#endif // LIST_LIBRARY_H
